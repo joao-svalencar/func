da8ab7d4af91fabd394a7c5bec08b409a958ad34
joao-svalencar <joaopaulo.valencar@usp.br>
2021-05-28 14:45:15 -0300

    First func commit

---
+++ R/func.R
@@ -0,0 +1,12 @@
+#' Surprise Function 
+#'
+#' @param x integer, any integer, usually a number from 1 to 10
+#'
+#' @return returns a message associated to the number provided in x
+#' @export
+#'
+func <- function(x)
+{
+  print(paste("O Naka foi um professor nota:", x))
+}
+
